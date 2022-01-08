8769f9179a0f1112a078558248c016198616edfc
james <t628x7600@gmail.com>
2022-01-08 17:36:03 +0800

    Not yet

---
+++ Dotnetty.Websocket.Demo/IWebsocketHandShakerAuth.cs
@@ -0,0 +1,16 @@
+﻿using DotNetty.Codecs.Http;
+
+namespace Dotnetty.Websocket.Demo;
+
+public interface IWebsocketHandShakerAuthorizer
+{
+    bool Auth(IFullHttpRequest request);
+}
+
+public class DefaultWebsocketHandShakerAuthorizer : IWebsocketHandShakerAuthorizer
+{
+    public bool Auth(IFullHttpRequest request)
+    {
+        return true;
+    }
+}
---
+++ Dotnetty.Websocket.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DotNetty.Codecs.Http;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Libuv;
+using Dotnetty.Websocket.Demo;
+
+var bossGroup = new DispatcherEventLoopGroup();
+var workGroup = new WorkerEventLoopGroup(bossGroup);
+var bootstrap = new ServerBootstrap();
+
+bootstrap.Group(bossGroup, workGroup)
+    .Channel<TcpServerChannel>()
+    .Option(ChannelOption.SoBacklog, 8192)
+    .ChildHandler(new ActionChannelInitializer<IChannel>(channel =>
+    {
+        IChannelPipeline pipeline = channel.Pipeline;
+        pipeline.AddLast(new HttpServerCodec());
+        pipeline.AddLast(new HttpObjectAggregator(65536));
+        pipeline.AddLast(new WebsocketHandShakerAuthHandler());
+    }));
+
+
+Console.WriteLine("Hello, World!");
